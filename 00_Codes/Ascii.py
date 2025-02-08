e3d8a24748d5ac7873d1f219f4889c9973bc4fb7
Arafat Sarkar <arafatgovt01@gmail.com>
2025-02-09 01:09:09 +0600

    Commit 01

---
+++ 00_Codes/Ascii.py
@@ -0,0 +1,9 @@
+#To convert an integer to ASCII we use char()
+# To convert a character to ASCII we use ord()
+
+String="You are in Anon3605 GitHUB"
+Integer=0
+for i in String:
+    print(str(ord(i)),end="")
+
+for ASCII String we keep the 
